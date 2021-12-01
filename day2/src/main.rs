7ab9a9026e1829ed884b67cc65a2bb86a76b6962
Jonas Malaco <jonas@protocubo.io>
2021-12-01 03:50:59 -0300

    day2: make base project

---
+++ day2/src/main.rs
@@ -0,0 +1,27 @@
+fn main()  {
+    println!("--- Day 2: Sonar Sweep ---");
+
+    let input = include_str!("../input.txt");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn example() {
+        let input = indoc! {"
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+        "};
+    }
+}
