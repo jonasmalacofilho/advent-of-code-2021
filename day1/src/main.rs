ee1f40f6f51daa05ac97d770c5a4714a7120d70a
Jonas Malaco <jonas@protocubo.io>
2021-12-01 02:34:30 -0300

    day1: add already complete and partially refactored solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,62 @@
-fn main() {
-    // let input = include_str!("../input.txt");
+use std::num::ParseIntError;
+
+fn main() -> Result<(), ParseIntError> {
+    let input = include_str!("../input.txt");
+    let depths = parse_depths(input)?;
+
+    println!("--- Day 1: Sonar Sweep ---");
+
+    println!("Measurements that increased: {}", increased(&depths));
+
+    println!(
+        "Measurements that increased (3-measurement window): {}",
+        increased_with_window(&depths)
+    );
+
+    Ok(())
+}
+
+fn parse_depths(input: &str) -> Result<Vec<usize>, ParseIntError> {
+    input
+        .lines()
+        .map(|line| line.parse::<usize>())
+        .collect::<Result<Vec<_>, _>>()
+}
+
+fn increased(depths: &[usize]) -> usize {
+    let mut prev = None;
+    let mut increased = 0;
+
+    for depth in depths {
+        if let Some(prev) = prev {
+            if depth > prev {
+                increased += 1;
+            }
+        }
+
+        prev = Some(depth);
+    }
+
+    increased
+}
+
+fn increased_with_window(depths: &[usize]) -> usize {
+    let mut prev = None;
+    let mut increased = 0;
+
+    for ((a, b), c) in depths.iter().zip(&depths[1..]).zip(&depths[2..]) {
+        let depth3 = a + b + c;
+
+        if let Some(prev) = prev {
+            if depth3 > prev {
+                increased += 1;
+            }
+        }
+
+        prev = Some(depth3);
+    }
+
+    increased
 }
 
 #[cfg(test)]
@@ -8,8 +65,37 @@ mod tests {
     use indoc::indoc;
 
     #[test]
-    fn example() {
+    fn parses_input() {
         let input = indoc! {"
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
         "};
+
+        assert_eq!(
+            parse_depths(input),
+            Ok(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263,])
+        );
+    }
+
+    #[test]
+    fn example() {
+        let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(increased(&depths), 7);
+    }
+
+    #[test]
+    fn example_with_window() {
+        let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(increased_with_window(&depths), 5);
     }
 }
