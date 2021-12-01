d0acd138e0f8c2688ce2301ddfd5ecd304126305
Jonas Malaco <jonas@protocubo.io>
2021-11-30 23:30:05 -0300

    day1: make base project

---
+++ day1/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // let input = include_str!("../input.txt");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn example() {
+        let input = indoc! {"
+        "};
+    }
+}
