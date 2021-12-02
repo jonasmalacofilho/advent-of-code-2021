9f60d84dd699131d371f4a012feb1c900c8dcea5
Jonas Malaco <jonas@protocubo.io>
2021-12-02 02:45:48 -0300

    day2: refactor with Command enum and fewer iterator chains

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,55 +1,78 @@
 fn main() {
-    println!("--- Day 2: ---");
+    println!("--- Day 2: Dive! ---");
 
     let input = include_str!("../input.txt");
     let data = parse(input);
 
-    println!("Part 1: {}", part1(&data));
-    println!("Part 2: {}", part2(&data));
+    println!("Final position: {}", final_position(&data));
+    println!("Final position, fixed: {}", final_position_fixed(&data));
 }
 
-type Data = Vec<(isize, isize)>;
+#[derive(Debug, PartialEq, Eq)]
+enum Command {
+    Forward(isize),
+    Down(isize),
+    Up(isize),
+}
 
-fn parse(input: &str) -> Data {
-    input.lines().map(|line| {
-        let mut parts = line.split(' ');
-        let command = parts.next().unwrap();
-        let amount = parts.next().unwrap().parse::<isize>().unwrap();
-        assert_eq!(parts.next(), None);
+fn parse(input: &str) -> Vec<Command> {
+    input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split(' ');
 
-        match command {
-            "forward" => (amount, 0),
-            "down" => (0, amount),
-            "up" => (0, -amount),
-            _ => panic!("unknown command: {}", command),
+            let command = parts.next().expect("missing command");
+            let amount = parts
+                .next()
+                .expect("missing amount")
+                .parse::<isize>()
+                .expect("cannot parse amount");
+
+            assert_eq!(parts.next(), None, "found trailing data");
+
+            match command {
+                "forward" => Command::Forward(amount),
+                "down" => Command::Down(amount),
+                "up" => Command::Up(amount),
+                _ => panic!("unknown command: {}", command),
+            }
+        })
+        .collect()
+}
+
+fn final_position(data: &[Command]) -> isize {
+    let mut hor = 0;
+    let mut dep = 0;
+
+    for cmd in data {
+        match cmd {
+            Command::Forward(x) => hor += x,
+            Command::Down(x) => dep += x,
+            Command::Up(x) => dep -= x,
         }
+    }
 
-
-
-    }).collect()
+    dbg!(hor, dep);
+    hor * dep
 }
 
-fn part1(data: &Data) -> isize {
-    let (h, d): (Vec<_>, Vec<_>) = data.iter().copied().unzip();
-    h.into_iter().sum::<isize>() * d.into_iter().sum::<isize>()
-}
-
-fn part2(data: &Data) -> isize {
+fn final_position_fixed(data: &[Command]) -> isize {
     let mut hor = 0;
     let mut dep = 0;
     let mut aim = 0;
 
-    for (f, a) in data.iter().copied() {
-        if f != 0 {
-            hor += f;
-            dep += aim * f;
-        } else {
-            aim += a;
+    for cmd in data {
+        match cmd {
+            Command::Forward(x) => {
+                hor += x;
+                dep += aim * x;
+            }
+            Command::Down(x) => aim += x,
+            Command::Up(x) => aim -= x,
         }
-
-        // dbg!((f, a, hor, dep, aim));
     }
 
+    dbg!(hor, dep, aim);
     hor * dep
 }
 
@@ -58,7 +81,7 @@ mod tests {
     use super::*;
     use indoc::indoc;
 
-    const SAMPLE: &'static str = indoc! {"
+    const SAMPLE: &str = indoc! {"
         forward 5
         down 5
         forward 8
@@ -69,25 +92,23 @@ mod tests {
 
     #[test]
     fn parses_the_input() {
-        assert_eq!(parse(SAMPLE), vec![
-            (5, 0),
-            (0, 5),
-            (8, 0),
-            (0, -3),
-            (0, 8),
-            (2, 0)
-        ]);
+        use Command::*;
+
+        assert_eq!(
+            parse(SAMPLE),
+            vec![Forward(5), Down(5), Forward(8), Up(3), Down(8), Forward(2)]
+        );
     }
 
     #[test]
     fn solves_the_first_example() {
         let data = parse(SAMPLE);
-        assert_eq!(part1(&data), 150);
+        assert_eq!(final_position(&data), 150);
     }
 
     #[test]
     fn solves_the_second_example() {
         let data = parse(SAMPLE);
-        assert_eq!(part2(&data), 900);
+        assert_eq!(final_position_fixed(&data), 900);
     }
 }
