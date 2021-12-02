ac2f5ba2e2d290af4e0fcd9531fe2525336dc306
Jonas Malaco <jonas@protocubo.io>
2021-12-02 02:28:35 -0300

    day2: add functional solutions

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,18 +8,49 @@ fn main() {
     println!("Part 2: {}", part2(&data));
 }
 
-type Data = Vec<String>;
+type Data = Vec<(isize, isize)>;
 
 fn parse(input: &str) -> Data {
-    input.lines().map(|line| line.to_owned()).collect()
+    input.lines().map(|line| {
+        let mut parts = line.split(' ');
+        let command = parts.next().unwrap();
+        let amount = parts.next().unwrap().parse::<isize>().unwrap();
+        assert_eq!(parts.next(), None);
+
+        match command {
+            "forward" => (amount, 0),
+            "down" => (0, amount),
+            "up" => (0, -amount),
+            _ => panic!("unknown command: {}", command),
+        }
+
+
+
+    }).collect()
 }
 
-fn part1(data: &Data) -> usize {
-    todo!()
+fn part1(data: &Data) -> isize {
+    let (h, d): (Vec<_>, Vec<_>) = data.iter().copied().unzip();
+    h.into_iter().sum::<isize>() * d.into_iter().sum::<isize>()
 }
 
-fn part2(data: &Data) -> usize {
-    todo!()
+fn part2(data: &Data) -> isize {
+    let mut hor = 0;
+    let mut dep = 0;
+    let mut aim = 0;
+
+    for (f, a) in data.iter().copied() {
+        if f != 0 {
+            hor += f;
+            dep += aim * f;
+        } else {
+            aim += a;
+        }
+
+        // dbg!((f, a, hor, dep, aim));
+    }
+
+    hor * dep
 }
 
 #[cfg(test)]
@@ -28,22 +59,35 @@ mod tests {
     use indoc::indoc;
 
     const SAMPLE: &'static str = indoc! {"
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
     "};
 
     #[test]
     fn parses_the_input() {
-        assert_eq!(parse(SAMPLE), Vec::<String>::new());
+        assert_eq!(parse(SAMPLE), vec![
+            (5, 0),
+            (0, 5),
+            (8, 0),
+            (0, -3),
+            (0, 8),
+            (2, 0)
+        ]);
     }
 
     #[test]
     fn solves_the_first_example() {
         let data = parse(SAMPLE);
-        assert_eq!(part1(&data), 42);
+        assert_eq!(part1(&data), 150);
     }
 
     #[test]
     fn solves_the_second_example() {
         let data = parse(SAMPLE);
-        assert_eq!(part2(&data), 42);
+        assert_eq!(part2(&data), 900);
     }
 }
