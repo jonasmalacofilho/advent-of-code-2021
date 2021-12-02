ff24da2c35b5e68e3f404a9af01dc61978cecadb
Jonas Malaco <jonas@protocubo.io>
2021-12-02 01:14:55 -0300

    day2: prepare assuming usual parse -> part1 -> part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,25 @@
-fn main()  {
-    println!("--- Day 2: Sonar Sweep ---");
+fn main() {
+    println!("--- Day 2: ---");
 
     let input = include_str!("../input.txt");
+    let data = parse(input);
+
+    println!("Part 1: {}", part1(&data));
+    println!("Part 2: {}", part2(&data));
+}
+
+type Data = Vec<String>;
+
+fn parse(input: &str) -> Data {
+    input.lines().map(|line| line.to_owned()).collect()
+}
+
+fn part1(data: &Data) -> usize {
+    todo!()
+}
+
+fn part2(data: &Data) -> usize {
+    todo!()
 }
 
 #[cfg(test)]
@@ -9,19 +27,23 @@ mod tests {
     use super::*;
     use indoc::indoc;
 
+    const SAMPLE: &'static str = indoc! {"
+    "};
+
     #[test]
-    fn example() {
-        let input = indoc! {"
-            199
-            200
-            208
-            210
-            200
-            207
-            240
-            269
-            260
-            263
-        "};
+    fn parses_the_input() {
+        assert_eq!(parse(SAMPLE), Vec::<String>::new());
+    }
+
+    #[test]
+    fn solves_the_first_example() {
+        let data = parse(SAMPLE);
+        assert_eq!(part1(&data), 42);
+    }
+
+    #[test]
+    fn solves_the_second_example() {
+        let data = parse(SAMPLE);
+        assert_eq!(part2(&data), 42);
     }
 }
