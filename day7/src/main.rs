4c3baf46e29f30c67bdc8e34828404a39c4baed0
Jonas Malaco <jonas@protocubo.io>
2021-12-07 03:02:48 -0300

    day7: share common alignment logic

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -6,11 +6,13 @@ fn main() -> Result<()> {
     let input = include_str!("../input.txt");
     let crabs = parse(input)?;
 
-    println!("Required fuel for alignment: {}", align(&crabs).1);
-    println!(
-        "Required fuel for alignment, fixed/v2: {}",
-        align_v2(&crabs).1
-    );
+    let alignment = align_linear(&crabs);
+    dbg!(&alignment);
+    println!("Required fuel for alignment: {}", alignment.1);
+
+    let alignment_v2 = align_v2(&crabs);
+    dbg!(&alignment_v2);
+    println!("Required fuel for alignment, fixed/v2: {}", alignment_v2.1);
 
     Ok(())
 }
@@ -26,41 +28,34 @@ fn parse(input: &str) -> Result<Vec<u32>> {
         .collect()
 }
 
-fn align(crabs: &[u32]) -> (u32, u32) {
-    let min = *crabs.iter().min().unwrap(); // FIXME
-    let max = *crabs.iter().max().unwrap(); // FIXME
+fn align(crabs: &[u32], cost: impl Fn(u32, u32) -> u32) -> (u32, u32) {
+    if crabs.is_empty() {
+        return (0, 0);
+    } else if crabs.len() == 1 {
+        return (crabs[0], 0);
+    }
+
+    let min = *crabs.iter().min().unwrap();
+    let max = *crabs.iter().max().unwrap();
 
     (min..max)
         .map(|pick| {
-            let cost = crabs
-                .iter()
-                .map(|&x| (x as i32 - pick as i32).abs())
-                .sum::<i32>() as u32;
-
+            let cost = crabs.iter().map(|&x| cost(x, pick)).sum::<u32>();
             (pick, cost)
         })
         .min_by_key(|(_, cost)| *cost)
-        .unwrap() // FIXME
+        .unwrap()
+}
+
+fn align_linear(crabs: &[u32]) -> (u32, u32) {
+    align(crabs, |x, pick| (x as i32 - pick as i32).abs() as u32)
 }
 
 fn align_v2(crabs: &[u32]) -> (u32, u32) {
-    let min = *crabs.iter().min().unwrap(); // FIXME
-    let max = *crabs.iter().max().unwrap(); // FIXME
-
-    (min..max)
-        .map(|pick| {
-            let cost = crabs
-                .iter()
-                .map(|&x| {
-                    let dist = (x as i32 - pick as i32).abs();
-                    (1..=dist).sum::<i32>()
-                })
-                .sum::<i32>() as u32;
-
-            (pick, cost)
-        })
-        .min_by_key(|(_, cost)| *cost)
-        .unwrap() // FIXME
+    align(crabs, |x, pick| {
+        let dist = (x as i32 - pick as i32).abs() as u32;
+        (1..=dist).sum::<u32>()
+    })
 }
 
 #[cfg(test)]
@@ -80,7 +75,7 @@ mod tests {
     fn gets_alignment_and_fuel_cost_correct() {
         let crabs = parse(SAMPLE).unwrap();
 
-        assert_eq!(align(&crabs), (2, 37));
+        assert_eq!(align_linear(&crabs), (2, 37));
     }
 
     #[test]
@@ -88,5 +83,14 @@ mod tests {
         let crabs = parse(SAMPLE).unwrap();
 
         assert_eq!(align_v2(&crabs), (5, 168));
+    }
+
+    #[test]
+    fn does_not_regress() {
+        let input = include_str!("../input.txt");
+        let crabs = parse(input).unwrap();
+
+        assert_eq!(align_linear(&crabs), (336, 344735));
+        // assert_eq!(align_v2(&crabs), (474, 96798233));
     }
 }
