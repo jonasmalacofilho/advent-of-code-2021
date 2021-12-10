9a089809be6b3de39653496ddf0e510bd69c7a84
Jonas Malaco <jonas@protocubo.io>
2021-12-10 03:03:40 -0300

    day10: solver part2 and clean up

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,13 +1,14 @@
-use eyre::{eyre, Report, Result, WrapErr};
+use eyre::{bail, Result};
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    println!("--- Day 10: Syntax Scoring ---");
 
     let lines = parse(INPUT)?;
 
-    dbg!(error_score(&lines));
+    println!("Error score: {}", error_score(&lines));
+    println!("Completion median score: {}", complete_middle_score(&lines));
 
     Ok(())
 }
@@ -15,45 +16,101 @@ fn main() -> Result<()> {
 const ALLOWED: &[u8] = b"()[]{}<>";
 
 fn parse(s: &str) -> Result<Vec<&[u8]>> {
-    // FIXME check chars
-    s.lines().map(|line| Ok(line.as_bytes())).collect()
+    s.lines()
+        .map(|line| {
+            let line = line.as_bytes();
+
+            for c in line {
+                if !ALLOWED.contains(c) {
+                    bail!("unsupported character: {}", char::from(*c));
+                }
+            }
+
+            Ok(line)
+        })
+        .collect()
 }
 
-fn corrupted<'a>(lines: &'a [&'a [u8]]) -> impl Iterator<Item = u8> + 'a {
-    lines.iter().zip(1..).filter_map(|(&line, lineno)| {
+fn close(c: u8) -> u8 {
+    match c {
+        b'(' => b')',
+        b'[' => b']',
+        b'{' => b'}',
+        b'<' => b'>',
+        _ => unreachable!(),
+    }
+}
+
+enum Analysis {
+    IllegalTerminator(u8),
+    MissingTerminators(Vec<u8>),
+    Fine,
+}
+
+fn analyze<'a>(lines: &'a [&'a [u8]]) -> impl Iterator<Item = Analysis> + 'a {
+    lines.iter().map(|&line| {
         let mut stack = vec![];
 
-        for c in line {
-            if b"([{<".contains(c) {
+        for &c in line {
+            if b"([{<".contains(&c) {
                 stack.push(c);
-            } else if b")]}>".contains(c) {
-                let start = stack.pop().unwrap();
-                match (start, c) {
-                    (b'(', b')') => { /* ok */ },
-                    (b'[', b']') => { /* ok */ },
-                    (b'{', b'}') => { /* ok */ },
-                    (b'<', b'>') => { /* ok */ },
-                    _ => return Some(*c)
-                };
+            } else if b")]}>".contains(&c) {
+                let open = stack.pop();
+                if open.is_none() || c != close(open.unwrap()) {
+                    return Analysis::IllegalTerminator(c);
+                }
             } else {
-                unreachable!();
+                panic!("unsupported character: {}", char::from(c));
             }
         }
 
-        None
+        if !stack.is_empty() {
+            let missing = stack.into_iter().rev().map(close).collect();
+            return Analysis::MissingTerminators(missing);
+        }
+
+        Analysis::Fine
     })
 }
 
 fn error_score(lines: &[&[u8]]) -> usize {
-    corrupted(lines)
-        .map(|illegal| match illegal {
-            b')' => 3,
-            b']' => 57,
-            b'}' => 1197,
-            b'>' => 25137,
-            _ => unreachable!(),
+    analyze(lines)
+        .map(|analysis| match analysis {
+            Analysis::IllegalTerminator(c) => match c {
+                b')' => 3,
+                b']' => 57,
+                b'}' => 1197,
+                b'>' => 25137,
+                _ => panic!("unsupported character: {}", char::from(c)),
+            },
+            _ => 0,
         })
         .sum()
+}
+
+fn complete_middle_score(lines: &[&[u8]]) -> usize {
+    let mut scores: Vec<_> = analyze(lines)
+        .filter_map(|analysis| match analysis {
+            Analysis::MissingTerminators(missing) => {
+                let score = missing.iter().fold(0, |acc, &c| {
+                    let pvalue = match c {
+                        b')' => 1,
+                        b']' => 2,
+                        b'}' => 3,
+                        b'>' => 4,
+                        _ => panic!("unsupported character: {}", char::from(c)),
+                    };
+                    acc * 5 + pvalue
+                });
+
+                Some(score)
+            }
+            _ => None,
+        })
+        .collect();
+
+    scores.sort_unstable();
+    scores[scores.len() / 2]
 }
 
 #[cfg(test)]
@@ -79,5 +136,20 @@ mod tests {
         let lines = parse(SAMPLE).unwrap();
 
         assert_eq!(error_score(&lines), 26397);
+    }
+
+    #[test]
+    fn calcutes_complete_scores() {
+        let lines = parse(SAMPLE).unwrap();
+
+        assert_eq!(complete_middle_score(&lines), 288957);
+    }
+
+    #[test]
+    fn does_not_regress() {
+        let lines = parse(INPUT).unwrap();
+
+        assert_eq!(error_score(&lines), 387363);
+        assert_eq!(complete_middle_score(&lines), 4330777059);
     }
 }
