91eac36a93e8bf56c30445330c6fd707ddca07b5
Jonas Malaco <jonas@protocubo.io>
2021-12-14 07:30:06 -0300

    day14: write a memory bounded alternative
    Uses a breath-first search; unfortunately, it is (even more) terribly
    slow for large numbers of steps (e.g. 40).

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, str};
+use std::{
+    collections::{BinaryHeap, HashMap},
+    time::{Duration, Instant},
+};
 
 const INPUT: &str = include_str!("../input.txt");
 
@@ -6,8 +9,19 @@ fn main() {
     println!("Hello, world!");
 
     let (template, rules) = parse(INPUT);
-    dbg!(score(&polymerize(template, &rules, 10)));
-    // dbg!(score(&polymerize(template, &rules, 40)));
+
+    dbg!(time(|| score(polymerize(template, &rules, 10).chars())));
+    dbg!(time(|| score(polymerize_mem_bounded(template, &rules, 10))));
+    dbg!(time(|| score(polymerize_mem_bounded(template, &rules, 40))));
+}
+
+fn time<F, T>(f: F) -> (T, Duration)
+where
+    F: Fn() -> T,
+{
+    let start = Instant::now();
+    let result = f();
+    (result, Instant::now() - start)
 }
 
 fn parse(s: &str) -> (&str, HashMap<&str, u8>) {
@@ -47,7 +61,7 @@ fn polymerize(template: &str, rules: &HashMap<&str, u8>, steps: usize) -> String
         buf.reserve(pre.len() * 2 - 1);
 
         for pair in pre.windows(2) {
-            let insert = rules[str::from_utf8(pair).unwrap()];
+            let insert = rules[std::str::from_utf8(pair).unwrap()];
 
             if first {
                 first = false;
@@ -65,10 +79,83 @@ fn polymerize(template: &str, rules: &HashMap<&str, u8>, steps: usize) -> String
     String::from_utf8(out).unwrap()
 }
 
-fn score(polymer: &str) -> usize {
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Pair {
+    step: u32,
+    ord: u64,
+    left: char,
+    right: char,
+}
+
+fn polymerize_mem_bounded(
+    template: &str,
+    rules: &HashMap<&str, u8>,
+    steps: usize,
+) -> impl Iterator<Item = char> {
+    let steps: u32 = steps as _;
+
+    let mut better_sules = HashMap::new();
+    for (&pair, &insert) in rules {
+        let mut chars = pair.chars();
+        let left = chars.next().unwrap();
+        let right = chars.next().unwrap();
+        better_sules.insert((left, right), insert.into());
+    }
+    let rules = better_sules;
+
+    let mut ord = u64::MAX;
+    let mut heap = BinaryHeap::new();
+    for (left, right) in template.chars().zip(template.chars().skip(1)) {
+        heap.push(Pair {
+            step: 0,
+            ord,
+            left,
+            right,
+        });
+        ord -= 1;
+    }
+
+    std::iter::from_fn(move || {
+        while let Some(Pair {
+            step, left, right, ..
+        }) = heap.pop()
+        {
+            if step >= steps {
+                if heap.is_empty() && step == steps {
+                    heap.push(Pair {
+                        step: u32::MAX,
+                        ord: 0,
+                        left: right,
+                        right,
+                    });
+                }
+                return Some(left);
+            } else {
+                let insert = rules[&(left, right)];
+                heap.push(Pair {
+                    step: step + 1,
+                    ord,
+                    left,
+                    right: insert,
+                });
+                heap.push(Pair {
+                    step: step + 1,
+                    ord: ord - 1,
+                    left: insert,
+                    right,
+                });
+                ord -= 2;
+            }
+        }
+        None
+    })
+}
+
+fn score(polymer: impl Iterator<Item = char>) -> usize {
     let mut counts: HashMap<char, usize> = HashMap::new();
 
-    for element in polymer.chars() {
+    for element in polymer {
+        // dbg!(element);
         let count = counts.entry(element).or_default();
         *count += 1;
     }
@@ -109,12 +196,23 @@ mod tests {
     #[test]
     fn sample_polymer_after_10_steps() {
         let (template, rules) = parse(SAMPLE);
-        assert_eq!(score(&polymerize(template, &rules, 10)), 1588);
+        assert_eq!(score(polymerize(template, &rules, 10).chars()), 1588);
+        assert_eq!(score(polymerize_mem_bounded(template, &rules, 10)), 1588);
+    }
+
+    #[test]
+    #[ignore] // requires, effectively, infinite time
+    fn sample_polymer_after_40_steps() {
+        let (template, rules) = parse(SAMPLE);
+        assert_eq!(
+            score(polymerize_mem_bounded(template, &rules, 40)),
+            2188189693529
+        );
     }
 
     #[test]
     fn does_not_regres() {
         let (template, rules) = parse(INPUT);
-        assert_eq!(score(&polymerize(template, &rules, 10)), 2797);
+        assert_eq!(score(polymerize(template, &rules, 10).chars()), 2797);
     }
 }
