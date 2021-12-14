f373c7331a3c082d3fcb00c0340142027e670ad7
Jonas Malaco <jonas@protocubo.io>
2021-12-14 08:37:30 -0300

    day14: clean up and document the algorithm for score_fast

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -24,29 +24,35 @@ where
     (result, Instant::now() - start)
 }
 
-fn parse(s: &str) -> (&str, HashMap<&str, u8>) {
+fn parse(s: &str) -> (&str, HashMap<&str, &str>) {
     let mut lines = s.lines();
 
     let template = lines.next().expect("missing template");
 
-    assert_eq!(lines.next().unwrap(), "");
+    assert_eq!(lines.next(), Some(""), "missing empty separator line");
 
     let mut rules = HashMap::new();
 
     for line in lines {
         let (pair, insert) = line.split_once(" -> ").expect("missing ` -> `");
-        assert_eq!(insert.as_bytes().len(), 1);
-        rules.insert(pair, insert.as_bytes()[0]);
+
+        assert_eq!(pair.chars().count(), 2, "pair must consist of two elements");
+        assert_eq!(insert.chars().count(), 1, "insert exactly one element");
+
+        rules.insert(pair, insert);
     }
 
     (template, rules)
 }
 
-fn polymerize(template: &str, rules: &HashMap<&str, u8>, steps: usize) -> String {
-    // To use `slice::windows()` we take advantage of knowing that all elements are (uppercase)
-    // ASCII letters; by asserting this here, we can simply unwrap any later fallible conversions
-    // from &[u8]/Vec<u8> to &str/String
-    assert!(template.is_ascii());
+fn polymerize(template: &str, rules: &HashMap<&str, &str>, steps: usize) -> String {
+    // We take advantage of knowing that all elements are (uppercase) ASCII letters, which allows
+    // us to use `.as_bytes().windows(2)`; by asserting that this hypothesis is indeed true, we can
+    // simply unwrap (or even use unchecked versions of) calls to `from_utf8`
+    assert!(template.is_ascii(), "only ASCII elements are supported");
+
+    // We also need to check that the rules only use ASCII
+    assert!(rules.iter().all(|(&k, &v)| k.is_ascii() && v.is_ascii()));
 
     let mut pre = template.as_bytes();
 
@@ -61,7 +67,7 @@ fn polymerize(template: &str, rules: &HashMap<&str, u8>, steps: usize) -> String
         buf.reserve(pre.len() * 2 - 1);
 
         for pair in pre.windows(2) {
-            let insert = rules[std::str::from_utf8(pair).unwrap()];
+            let insert = rules[std::str::from_utf8(pair).unwrap()].as_bytes()[0];
 
             if first {
                 first = false;
@@ -87,32 +93,37 @@ fn score(polymer: impl Iterator<Item = char>) -> usize {
         *count += 1;
     }
 
-    let most_common = counts.iter().max_by_key(|&(_, c)| c).unwrap();
-    let least_common = counts.iter().min_by_key(|&(_, c)| c).unwrap();
-
-    most_common.1 - least_common.1
+    let most_common = counts.values().max().unwrap();
+    let least_common = counts.values().min().unwrap();
+    most_common - least_common
 }
 
-fn score_fast(template: &str, rules: &HashMap<&str, u8>, steps: usize) -> usize {
-    // To use `slice::windows()` we take advantage of knowing that all elements are (uppercase)
-    // ASCII letters; by asserting this here, we can simply unwrap any later fallible conversions
-    // from &[u8]/Vec<u8> to &str/String
-    assert!(template.is_ascii());
+fn score_fast(template: &str, rules: &HashMap<&str, &str>, steps: usize) -> usize {
+    // We take advantage of knowing that all elements are (uppercase) ASCII letters, which allows
+    // us to use `.as_bytes().windows(2)`; by asserting that this hypothesis is indeed true, we can
+    // simply unwrap (or even use unchecked versions of) calls to `from_utf8`
+    assert!(template.is_ascii(), "only ASCII elements are supported");
 
+    // We also need to check that the rules only use ASCII
+    assert!(rules.iter().all(|(&k, &v)| k.is_ascii() && v.is_ascii()));
+
+    // First, count all pairs in the `template` polymer
     let mut pairs: HashMap<(u8, u8), usize> = HashMap::new();
-
     for pair in template.as_bytes().windows(2) {
         let count = pairs.entry((pair[0], pair[1])).or_default();
         *count += 1;
     }
 
+    // Next, go over each polymerization step, according to the `rules`, but only keep track of how
+    // many of each pair of elements is in the polymer
     for _step in 0..steps {
         let mut new_pairs = HashMap::new();
         for (&pair, &count) in pairs.iter() {
             let pair_arr = [pair.0, pair.1];
             let pair_str = std::str::from_utf8(&pair_arr).unwrap();
 
-            let insert = rules[pair_str];
+            let insert = rules[pair_str].as_bytes()[0];
+
             let left = (pair.0, insert);
             let right = (insert, pair.1);
 
@@ -124,21 +135,24 @@ fn score_fast(template: &str, rules: &HashMap<&str, u8>, steps: usize) -> usize 
         pairs = new_pairs;
     }
 
-    let mut elements: HashMap<char, usize> = HashMap::new();
-
+    // Count the how many times the *first* element of each pair appears; this is *almost* the
+    // final element count...
+    let mut elements: HashMap<u8, usize> = HashMap::new();
     for (pair, count) in pairs {
-        let elem_count = elements.entry(pair.0.into()).or_default();
+        let elem_count = elements.entry(pair.0).or_default();
         *elem_count += count;
     }
 
+    // ...except for the very last element in the polymer chain, which we can just take from the
+    // original template
     *elements
-        .entry(template.chars().last().unwrap())
+        .entry(*template.as_bytes().last().unwrap())
         .or_default() += 1;
 
-    let most_common = elements.iter().max_by_key(|&(_, c)| c).unwrap();
-    let least_common = elements.iter().min_by_key(|&(_, c)| c).unwrap();
-
-    most_common.1 - least_common.1
+    // Finally, find the most and least common elements and compute the score
+    let most_common = elements.values().max().unwrap();
+    let least_common = elements.values().min().unwrap();
+    most_common - least_common
 }
 
 #[cfg(test)]
@@ -185,5 +199,6 @@ mod tests {
         let (template, rules) = parse(INPUT);
         assert_eq!(score(polymerize(template, &rules, 10).chars()), 2797);
         assert_eq!(score_fast(template, &rules, 40), 2926813379532);
+        assert_eq!(score_fast(template, &rules, 10), 2797);
     }
 }
